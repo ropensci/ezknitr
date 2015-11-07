bbd9eb2f3a0abf7a7c0a7cec59cfe4c3c55618dd
Dean Attali <daattali@gmail.com>
2015-11-07 12:48:51 -0800

    simplify setup_ezspin_test()

--- R/setup_ezspin_test.R
+++ R/setup_ezspin_test.R
@@ -8,59 +8,49 @@
 #'
 #' The console output from this function will give more specific insturctions on
 #' how to interact with this test directory.
-#' @param loc The path where you want to set up the test directories.
-#' @return The path to the root of the test directory.
 #' @export
 #' @examples
 #' \dontrun{
 #' library(ezknitr)
-#' tmp <- setup_ezspin_test("~")
-#' origwd <- setwd(tmp)
-#' knitr::spin("container/R/ezspin_test.R")
-#' setup_ezspin_test("..")
-#' ezspin("R/ezspin_test.R", wd = "container")
-#' setup_ezspin_test("..")
-#' ezspin("R/ezspin_test.R", wd = "container",
+#' setup_ezspin_test()
+#' knitr::spin("R/ezspin_test.R")
+#' setup_ezspin_test()
+#' ezspin("R/ezspin_test.R", wd = "ezknitr_test")
+#' setup_ezspin_test()
+#' ezspin("R/ezspin_test.R", wd = "ezknitr_test",
 #'         out_dir = "output", fig_dir = "coolplots")
 #' setwd(origwd)
 #' unlink(tmp, recursive = TRUE, force = TRUE)
 #' }
 #' @seealso \code{\link[ezknitr]{ezspin}}
 #' @seealso \code{\link[knitr]{spin}}
-setup_ezspin_test <- function(loc = getwd()) {
+setup_ezspin_test <- function() {
   s <- try(
     system.file("examples", "ezspin_test.R", package = "ezknitr", mustWork = TRUE),
     silent = TRUE)
   if (class(s) == "try-error") {
     stop("Could not find example file")
   }
-
-  testdir <- file.path(loc, "ezspin_test")
-  unlink(list.files(testdir), recursive = TRUE, force = TRUE)
-
-  container <- file.path(testdir, "container")
-  rdir <- file.path(container, "R")
-  datadir <- file.path(container, "data")
+  
+  testdir <- file.path("ezknitr_test")
+  unlink(list.files(testdir, full.names = TRUE), recursive = TRUE, force = TRUE)
+  
+  rdir <- file.path(testdir, "R")
+  datadir <- file.path(testdir, "data")
   dir.create(rdir, showWarnings = FALSE, recursive = TRUE)
   dir.create(datadir, showWarnings = FALSE)
-
+  
   file.copy(from = s, to = rdir)
   cat("10 20 30", file = file.path(datadir, "numbers.txt"))
-
-  message(paste0("ezspin demo was set up at\n", normalizePath(testdir), "\n\n",
-                 "To experiment with ezspin, set that as your working directory and ",
-                 "run following commands. After each command, look where all the ",
+  
+  message(paste0("`ezspin` demo was set up at\n", normalizePath(testdir), "\n\n",
+                 "To experiment with `ezspin`, run the following command. ",
+                 "After each command, look where all the ",
                  "output was created and look at the resulting output to see ",
                  "the differences.\n"))
-
-  message('   1.   knitr::spin("container/R/ezspin_test.R")')
-  message('   2.   ezknitr::ezspin("R/ezspin_test.R", wd = "container")')
-  message('   3.   ezknitr::ezspin("R/ezspin_test.R", wd = "container",')
+  
+  message('   1.   knitr::spin("ezknitr_test/R/ezspin_test.R")')
+  message('   2.   ezknitr::ezspin("R/ezspin_test.R", wd = "ezknitr_test")')
+  message('   3.   ezknitr::ezspin("R/ezspin_test.R", wd = "ezknitr_test",')
   message('                        out_dir = "output", fig_dir = "coolplots")')
-
-  message(paste0('\nNote: to start with a clean state after each of the above ',
-                 'commands, run `ezknitr::setup_ezspin_test("..")` to set up the demo ',
-                 'directories again.'))
-
-  invisible(testdir)
 }
