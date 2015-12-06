57bf25621302648a4fb67c076d2b5092a6402a08
Dean Attali <daattali@gmail.com>
2015-12-06 03:23:55 -0800

    fix broken tests

--- tests/testthat/test-ezknitr.R
+++ tests/testthat/test-ezknitr.R
@@ -7,7 +7,7 @@ test_that("ezknit creates the correct files", {
     file.path(tmp, "R", "ezknit_test.Rmd"),
     file.path(tmp, "R", "ezknit_test.md"),
     file.path(tmp, "R", "ezknit_test.html"),
-    file.path(tmp, "R", "ezknit_test", "noecho-1.png"),
+    file.path(tmp, "R", "ezknit_test", "plot-1.png"),
     file.path(tmp, "data", "numbers.txt")
   )
   expect_true(all(file.exists(files)))
@@ -20,7 +20,7 @@ test_that("ezknit creates the correct files", {
     file.path(tmp, "R", "ezknit_test.Rmd"),
     file.path(tmp, "output", "ezknit_test.md"),
     file.path(tmp, "output", "ezknit_test.html"),
-    file.path(tmp, "output", "coolplots", "noecho-1.png"),
+    file.path(tmp, "output", "coolplots", "plot-1.png"),
     file.path(tmp, "data", "numbers.txt")
   )
   expect_true(all(file.exists(files)))
@@ -34,7 +34,7 @@ test_that("ezspin creates the correct files", {
     file.path(tmp, "R", "ezspin_test.R"),
     file.path(tmp, "R", "ezspin_test.md"),
     file.path(tmp, "R", "ezspin_test.html"),
-    file.path(tmp, "R", "ezspin_test", "noecho-1.png"),
+    file.path(tmp, "R", "ezspin_test", "plot-1.png"),
     file.path(tmp, "data", "numbers.txt")
   )
   expect_true(all(file.exists(files)))
@@ -48,7 +48,7 @@ test_that("ezspin creates the correct files", {
     file.path(tmp, "output", "ezspin_test.Rmd"),
     file.path(tmp, "output", "ezspin_test.md"),
     file.path(tmp, "output", "ezspin_test.html"),
-    file.path(tmp, "output", "coolplots", "noecho-1.png"),
+    file.path(tmp, "output", "coolplots", "plot-1.png"),
     file.path(tmp, "data", "numbers.txt")
   )
   expect_true(all(file.exists(files)))
